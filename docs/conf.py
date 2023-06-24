36f6b563629f046619028a837bb5e886264abe2d
Henrik Boström <henrikbostrom@users.noreply.github.com>
2023-06-24 23:23:35 +0200

    0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,10 @@ copyright = '2023, Henrik Boström'
 author = 'Henrik Boström'
 
 # The short X.Y version
-version = '0.5.0'
+version = '0.5.1'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.0'
+release = '0.5.1'
 
 
 # -- General configuration ---------------------------------------------------
