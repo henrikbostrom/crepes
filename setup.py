dfc51e4e799043ed51719cddc643b6d5786ce2ae
Henrik Boström <henrikbostrom@users.noreply.github.com>
2025-03-31 15:15:54 +0200

    0.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="crepes",
-    version="0.7.1",
+    version="0.8.0",
     author="Henrik Boström",
     author_email="bostromh@kth.se",
     description="Conformal classifiers, regressors, and predictive systems (crepes)",
@@ -22,6 +22,6 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    install_requires=["numpy", "pandas"],
+    install_requires=["numpy", "pandas", "scipy"],
     python_requires=">=3.8",
 )
